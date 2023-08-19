2603f6628845e39587a985f3de495a72a52110c6
edr3x <dhunganaannanta@gmail.com>
2023-08-19 13:09:48 +0545

    feat: security headers

--- main.go
+++ main.go
@@ -48,6 +48,18 @@ func main() {
 		})
 	})
 
+	// Security Headers
+	app.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("X-Content-Type-Options", "nosniff")
+			w.Header().Set("X-XSS-Protection", "1; mode=block")
+			w.Header().Set("X-Frame-Options", "SAMEORIGIN")
+			w.Header().Set("Strict-Transport-Security", "max-age=5184000; includeSubDomains")
+
+			next.ServeHTTP(w, r)
+		})
+	})
+
 	app.Get("/{name}", func(w http.ResponseWriter, r *http.Request) {
 		name := chi.URLParam(r, "name")
 		if name == "err" {
